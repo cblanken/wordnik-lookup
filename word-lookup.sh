64589dc79b76a10222466478aed1cdd55a377f7f
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-05-14 10:42:16 -0400

    Output message when no pronunciation info is available

--- word-lookup.sh
+++ word-lookup.sh
@@ -2,24 +2,37 @@
 
 # Set working directory
 cd "$(dirname "$(realpath "$0")")" || exit
-# Source necessary config files
+
+# Source api key
 . ./api_key
 
 word="$1"
+
+# Query API for word data
 defs_json=$(curl -s -X GET --header Accept: application/json https://api.wordnik.com/v4/word.json/"$word"/definitions?api_key="$api_key")
 def_exists=$(jq '.error' 2>/dev/null <<<"$defs_json")
 if [ "$def_exists" = "\"Not Found\"" ]; then
     echo "No definitions exist on Wordnik for $word"
     exit 1
 fi
-
 pronunciations_json=$(curl -s -X GET --header Accept: application/json https://api.wordnik.com/v4/word.json/"$word"/pronunciations?api_key=$api_key)
-pronunciations=$(echo "$pronunciations_json" | jq 'map(select(.rawType == "IPA"))' 2>/dev/null | jq -r '["PRONUNCIATION", "ALPHABET"], ["-------------", "--------"], (.[] | [.raw, .rawType]) | @tsv')
-defs=$(echo "$defs_json" | jq -r '["SPEECH","DEFINITION"], ["------","----------"], (.[] | [.partOfSpeech, .text]) | @tsv' 2>/dev/null | sed 's/<[^>]*>//g')
+
+# Parse definition data from json
+defs=$(echo "$defs_json" \
+    | jq -r '["SPEECH","DEFINITION"], ["------","----------"], (.[] | [.partOfSpeech, .text]) | @tsv' 2>/dev/null \
+    | sed 's/<[^>]*>//g')
+pronunciations=$(echo "$pronunciations_json" \
+    | jq 'map(select(.rawType == "IPA"))' 2>/dev/null \
+    | jq -r '["PRONUNCIATION", "ALPHABET"], ["-------------", "--------"], (.[] | [.raw, .rawType]) | @tsv')
+
 print_defs() {
     echo -e "WORD\n----\n$word\n"
     # TODO: add top example
-    echo "$pronunciations" | head -n5
+    if -n "$pronunciations"; then
+        echo "$pronunciations" | head -n5
+    else
+        echo "No pronunciations found for \"$word\"."
+    fi
     echo ""
     echo "$defs"
     echo ""
