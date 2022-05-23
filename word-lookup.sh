770cb3fae6367cd0208f724ee3fac8e4602a9974
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-05-23 12:46:36 -0400

    Refactor to properly columnize output

--- word-lookup.sh
+++ word-lookup.sh
@@ -1,44 +1,68 @@
 #!/bin/bash
 
+# NOTE: `column-new` is an updated version of column binary from util-linux-2.35.
+#       Ubuntu has an outdated version by default and doesn't allow for easy 
+#       installation via apt, so it must be compiled from source.
+
 # Set working directory
 cd "$(dirname "$(realpath "$0")")" || exit
 
 # Source api key
 . ./api_key
 
-word="$1"
-
 # Query API for word data
+word="$1"
 defs_json=$(curl -s -X GET --header Accept: application/json https://api.wordnik.com/v4/word.json/"$word"/definitions?api_key="$api_key")
-def_exists=$(jq '.error' 2>/dev/null <<<"$defs_json")
+def_exists=$(jq '.error' 2>/dev/null <<< "$defs_json")
 if [ "$def_exists" = "\"Not Found\"" ]; then
     echo "No definitions exist on Wordnik for $word"
     exit 1
 fi
-pronunciations_json=$(curl -s -X GET --header Accept: application/json https://api.wordnik.com/v4/word.json/"$word"/pronunciations?api_key=$api_key)
 
 # Parse definition data from json
 defs=$(echo "$defs_json" \
-    | jq -r '["SPEECH","DEFINITION"], ["------","----------"], (.[] | [.partOfSpeech, .text]) | @tsv' 2>/dev/null \
+    | jq -r '(.[] | [.partOfSpeech, .text]) | @tsv' 2>/dev/null \
     | sed 's/<[^>]*>//g')
+
+# Parse pronunciation data from json
+pronunciations_json=$(curl -s -X GET --header Accept: application/json https://api.wordnik.com/v4/word.json/"$word"/pronunciations?api_key=$api_key)
 pronunciations=$(echo "$pronunciations_json" \
     | jq 'map(select(.rawType == "IPA"))' 2>/dev/null \
-    | jq -r '["PRONUNCIATION", "ALPHABET"], ["-------------", "--------"], (.[] | [.raw, .rawType]) | @tsv')
+    | jq -r '(.[] | [.raw, .rawType]) | @tsv')
 
-print_defs() {
-    echo -e "WORD\n----\n$word\n"
+println() {
+    printf "%s\t%s\n" "$1" "$2"
+}
+
+split=("--------------" "----------")
+print_all() {
+    println "WORD: $word"
+    println ${split[0]} ${split[1]}
+    println "PRONUNCIATION" "ALPHABET"
+    println ${split[0]} ${split[1]}
     # TODO: add top example
-    if -n "$pronunciations"; then
-        echo "$pronunciations" | head -n5
+    #echo -n "$pronunciations" | tail -n +2
+    if [ "$pronunciations" != "" ]; then
+        echo "$pronunciations" | head -n 5
+        echo ""
     else
-        echo "No pronunciations found for \"$word\"."
+        println "" "No pronunciations found for \"$word\""
     fi
-    echo ""
-    echo "$defs"
-    echo ""
+    println ${split[0]} ${split[1]}
+    println "SPEECH" "DEFINITION"
+    println ${split[0]} ${split[1]}
+    printf "%b\n" "$defs"
 }
 
 # Output definition
-print_defs | less -cS
+print_all | column-new \
+    --table \
+    --separator $'\t' \
+    --output-width 90 \
+    --table-noheadings \
+    --table-columns C1,C2 \
+    --table-wrap C2 \
+    --table-empty-lines \
+    | less -cS
 exit 0
 
